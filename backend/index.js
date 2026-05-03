7412c834f753ebe9e31c78bb51dc29c1281afd85
Ashutosh Zope <ashutosh@Ashutoshs-MacBook-Air.local>
2026-05-02 21:23:50 -0500

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
 app.use(express.json({ limit: '5mb' }));
